c42483bae3e09b84a8782d82486737a43f9b5ba7
George <gaoshiming@live.com>
2013-05-18 17:54:46 +0800

    Signed-off-by: George <gaoshiming@live.com>

--- public/app/modules/views/loginShow.js
+++ public/app/modules/views/loginShow.js
@@ -6,22 +6,21 @@ define(['backbone','jquery','lodash','raphael','text!templates/loginShow.html'],
 
 	},
 	events:{
-	    'load #loginSlide img':'afterRender'
+
 	},
 	afterRender: function(){
-	    var R = Raphael('loginSlide',640,480), img, anim, angel = 360;
-	    img = R.image('images/1.jpg',16,12,32,24);
+	  var R = Raphael('loginSlide',640,480), img, anim, angel = 360;
+	  img = R.image('images/1.jpg',16,12,32,24);
           img.attr({opacity:0});
-	    anim = Raphael.animation({
-              '30%':{transform:'r2160',opacity:1,x:160,y:120,width:320,height:240},
-              '70%':{},
-              '100%':{transform:'r0',opacity:0,x:624,y:468,width:32,height:24}},5e3,'',function(){
-		var n = Math.random() * 5 | 0 + 1;
-		this.attr({src : 'images/'+n+'.jpg'});
+	  anim = Raphael.animation({
+            '30%':{transform:'r2160',opacity:1,x:160,y:120,width:320,height:240},
+            '70%':{},
+            '100%':{transform:'r0',opacity:0,x:624,y:468,width:32,height:24}},5e3,'elastic',function(){
+	      var n = Math.random() * 5 | 0 + 1;
+	      this.attr({src : 'images/'+n+'.jpg'});
 	    });
 
-	    img.animate(anim.repeat(Infinity));
-
+	  img.animate(anim.repeat(Infinity));
 
 	}
 
